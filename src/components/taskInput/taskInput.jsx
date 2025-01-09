3bcd5bae34c1219950bafa38e473454019c8b53f
OlivierCodes <datasciences1010@gmail.com>
2025-01-09 20:19:04 +0100

    commit

---
+++ src/components/taskInput/taskInput.jsx
@@ -0,0 +1,19 @@
+// Ce composant est utilisé pour afficher le champ de saisie de tâche.
+import styles from "./taskInput.module.css";
+export const TaskInput = () => {
+  return (
+    <div className={`box ${styles.element}`}>
+      <h2 className={styles.title}>🎯 Ajoute ta prochaine tache</h2>
+      <form className={styles.container}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Indiquer un titre de tache explicite."
+        />
+        <button className="button-primary" type="submit">
+          Ajouter
+        </button>
+      </form>
+    </div>
+  );
+};
